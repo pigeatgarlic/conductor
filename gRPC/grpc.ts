60e0451fc5b19a894c94140d5e48d1525a4cfc0d
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-01-27 16:53:27 +0700

    start loop

--- gRPC/grpc.ts
+++ gRPC/grpc.ts
@@ -1,8 +1,6 @@
 import * as grpc from "@grpc/grpc-js"
 import * as loader from "@grpc/proto-loader"
 import { ProtoGrpcType } from "./generated/define"
-import { AuthRequest } from "./generated/protobuf/AuthRequest";
-import { AuthResult } from "./generated/protobuf/AuthResult";
 import { StreamServiceHandlers } from "./generated/protobuf/StreamService";
 import { WorkerCommand } from "./generated/protobuf/WorkerCommand";
 import { WorkerStatus } from "./generated/protobuf/WorkerStatus";
@@ -11,19 +9,40 @@ import { WorkerStatus } from "./generated/protobuf/WorkerStatus";
 
 
 const exampleServer: StreamServiceHandlers = {
-    StreamRequest( call: grpc.ServerDuplexStream<WorkerStatus, WorkerCommand>,) {
-		call.metadata.get("authorization")
+    StreamRequest(call: grpc.ServerDuplexStream<WorkerStatus, WorkerCommand>,) {
+		const auth = call.metadata.get("authorization");
+
+		const readLoop = (async (): Promise<void> => {
+			while(true) {
+				const status = call.read()
+
+
+			}
+		})
+		const writeLoop = (async (): Promise<void> => {
+			const command = {
+				command: null,
+				data: null
+			}
+
+			while(true) {
+
+
+				call.write(command);
+			}
+		})
+
+		readLoop();
+		writeLoop();
     },
-    Auth(call : grpc.ServerUnaryCall<AuthRequest,AuthResult>){
-    }
 }
 
 export function getServer(): grpc.Server {
-  const packageDefinition = loader.loadSync('./proto/define.proto');
-  const proto = (grpc.loadPackageDefinition(
-    packageDefinition
-  ) as unknown) as ProtoGrpcType;
-  const server = new grpc.Server();
-  server.addService(proto.protobuf.StreamService.service, exampleServer);
-  return server;
+	const packageDefinition = loader.loadSync('./proto/define.proto');
+	const proto = (grpc.loadPackageDefinition(
+		packageDefinition
+	) as unknown) as ProtoGrpcType;
+	const server = new grpc.Server();
+	server.addService(proto.protobuf.StreamService.service, exampleServer);
+	return server;
 }
