f0586933e4fa5f4dc8fd9cff62785f5283b0feaf
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-02-16 18:42:59 +0700

    add edge-turn

--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
 import * as grpc from "@grpc/grpc-js"
 import {getServer} from "./gRPC/grpc"
 import * as dotenv from 'dotenv' 
+import { TurnServer } from "edge-turn/dist/index"
 dotenv.config()
+
+
+const turn = new TurnServer("huyhoang","huyhoang",3478,(log) => {
+    console.log(`message from turn server : ${log}`)
+});
+turn.Start()
+
+
 
 
 const host = '0.0.0.0:9090';
